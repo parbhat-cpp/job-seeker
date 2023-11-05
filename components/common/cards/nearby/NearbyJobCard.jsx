23881e3e9adb7a83104293ad5090df11e38bac39
parbhat-cpp <prabhat29ps@gmail.com>
2023-11-05 13:16:34 +0530

    first commit

---
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import styles from "./nearbyjobcard.style";
+
+import { View, Text, TouchableOpacity, Image } from "react-native";
+
+import { checkImageURL } from "../../../../utils";
+
+const NearbyJobCard = ({ job, handleNavigate }) => {
+  return (
+    <TouchableOpacity style={styles.container} onPress={handleNavigate}>
+      <TouchableOpacity style={styles.logoContainer}>
+        <Image
+          source={{
+            uri: checkImageURL(job.employer_logo)
+              ? job.employer_logo
+              : "https://pikbest.com/png-images/job-icon-vector-graphics_1786897.html",
+          }}
+          resizeMode="contain"
+          style={styles.logoImage}
+        />
+      </TouchableOpacity>
+
+      <View style={styles.textContainer}>
+        <Text style={styles.jobName} numberOfLines={1}>
+          {job.job_title}
+        </Text>
+        <Text style={styles.jobType}>{job.job_employment_type}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default NearbyJobCard;
